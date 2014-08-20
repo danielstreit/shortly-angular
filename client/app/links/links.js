edb8952ac5816262a862ec04dc16567f5dafbcb3
danielstreit <daniel.streit@gmail.com>
2014-08-20 16:18:12 -0700

    Finished links controller, template, routes

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,12 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  $scope.getLinks = function() {
+    Links.getLinks.then(function(res){
+      console.log('response:', res.data);
+      $scope.data.links = res.data;
+    });
+  };
+  $scope.getLinks();
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,13 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getLinks = $http({
+    method: 'GET',
+    url: '/api/links'
+  });
+  return {
+    getLinks: getLinks
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
